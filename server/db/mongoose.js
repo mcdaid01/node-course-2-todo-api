ab98424b04292468cd03b3fa3c0cb813e938c568
Mike McDaid <mcdaid01@gmail.com>
2017-08-14 09:21:45 +0100

    app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise; // using mongoose own promise library
-mongoose.connect("mongodb://localhost:27017/TodoApp",{
+console.log(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI ||  "mongodb://localhost:27017/TodoApp",{
     useMongoClient: true
 });
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -21,7 +21,7 @@ beforeEach((done) => {
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
-    var text = 'Test todo text';
+    const text = 'Test todo text';
 
     request(app)
       .post('/todos')
